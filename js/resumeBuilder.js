a4d0fe70f5a83fd5c2a6d3a70c29334da96938de
armaango <armaan.goyal@gmail.com>
2016-07-16 23:35:18 -0400

    initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append("Armaan Goyal");
+ var myName = "Armaan Goyal";
+ var awsomeThoughts = "I am awsome";
+ console.log (awsomeThoughts + myName);
+ var funThoughts = awsomeThoughts.replace("awsome", "FUNNY");
+ var formattedName = HTMLheaderName.replace("%data%","Armaan Goyal");
+ var formattedRole = HTMLheaderRole.replace("%data%","Software Developer");
+
+ 
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
