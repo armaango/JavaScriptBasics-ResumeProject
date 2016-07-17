4f4f7aef57433c645c90db2891a37c186981ab34
armaango <armaan.goyal@gmail.com>
2016-07-16 23:58:23 -0400

    modified work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,3 +12,28 @@ This is empty on purpose! Your code to build the resume will go here.
  
  $("#header").prepend(formattedRole);
  $("#header").prepend(formattedName);
+
+ var bio = {
+ 	"name":myName,
+ 	"age":32,
+ 	"role":"Software Developer",
+ 	"contactInfo":"7264329772",
+ 	"pictureUrl":"#",
+ 	"skills":["java","php","c++"],
+ 	"welcomeMessage":"Ola"
+ }
+var work = {
+	jobs:[
+	{
+		"employer":"Infosys Limited",
+		"title":"Systems Engineer",
+		"dates":"March 2013-April 2015",
+		"description":"Worked as a developed and later as a Business Analyst for 3 major US based Clients, namely, AMEX, Boeing and Aetna Inc."
+	},
+	{
+		"employer":"University at Buffalo",
+		"title":"Graduate Researcher",
+		"dates":"May 2016-Present",
+		"description":"Working on a Research project to collect cellular data through crowdsourcing by building and deploying an Android app"
+	}]
+}
